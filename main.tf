56c495550f713f8b6bf73d2f7b685869d281dfc6
Adam Coulter <adam.coulter@servian.com>
2020-08-13 22:58:20 +1000

    removed provider blocks from reusable module

--- main.tf
+++ main.tf
@@ -5,10 +5,15 @@ locals {
   }
 }
 
-# Note that the subscription cannot be programmatically deleted, 
-# it is just removed from Terraform's state file
+data "azurerm_client_config" "current" {}
+
+data "external" "az_client_config" {
+  program = ["pwsh", "-command", "@{tenant=$env:ARM_TENANT_ID;client=$env:ARM_CLIENT_ID;secret=$env:ARM_CLIENT_SECRET} | ConvertTo-Json | Write-Output"]
+}
+
 resource "shell_script" "subscription" {
   working_directory = path.module
+  interpreter = ["pwsh", "-command"]
   lifecycle_commands {
     create = ". ./subscription.ps1; Create"
     read   = ". ./subscription.ps1; Read"
@@ -18,5 +23,10 @@ resource "shell_script" "subscription" {
   environment = {
     name = var.name
     type = local.type_codes[var.type]
+  }
+  sensitive_environment = {
+    azsub_client_id     = data.external.az_client_config.result.client
+    azsub_client_secret = data.external.az_client_config.result.secret
+    azsub_tenant_id     = data.azurerm_client_config.current.tenant_id
   }
 }
--- versions.tf
+++ versions.tf
@@ -9,23 +9,3 @@ terraform {
     }
   }
 }
-
-provider "azurerm" {
-  features {}
-}
-
-data "azurerm_client_config" "current" {}
-
-data "external" "az_client_config" {
-  program = ["pwsh", "-command", "@{tenant=$env:ARM_TENANT_ID;client=$env:ARM_CLIENT_ID;secret=$env:ARM_CLIENT_SECRET} | ConvertTo-Json | Write-Output"]
-}
-
-provider "shell" {
-  interpreter = ["pwsh", "-command"]
-
-  sensitive_environment = {
-    azsub_client_id     = data.external.az_client_config.result.client
-    azsub_client_secret = data.external.az_client_config.result.secret
-    azsub_tenant_id     = data.azurerm_client_config.current.tenant_id
-  }
-}
